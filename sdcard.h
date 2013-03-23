3133aa4c121c456cfcad8916fabc3409cdc1ca83
Torrentula <orwar@web.de>
2013-03-23 18:43:30 +0100

    added read block function

--- sdcard.h
+++ sdcard.h
@@ -15,6 +15,7 @@ extern unsigned char SDWdata[512];
 
 void SDcard_init(void);
 void SDcard_read_block(unsigned long address);
+void SDcard_write_block(unsigned long address);
 unsigned char SDcard_get_response(unsigned char response);
 
 #endif	/* SDCARD_H */
